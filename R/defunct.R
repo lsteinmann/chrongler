4d7253817b7482b71761ed127639396b282ded63
Lisa Steinmann <lisa.steinmann@dainst.de>
2026-03-19 18:46:58 +0100

    remove funs for idai.chronontology

---
+++ R/defunct.R
@@ -0,0 +1,20 @@
+#' (Removed) Get Data for One Period from iDAI.chronontology
+#' This function has been removed from chrongler.
+#'
+#' @param id (chr)
+#' @return Nothing.
+#' @export
+from_chronontology <- function(id = "") {
+  .Defunct(msg = "'from_chronontology()' has been removed from this package.",
+           package = "chrongler")
+}
+#' (Removed) Basic Query for iDAI.chronontology
+#' This function has been removed from chrongler.
+#'
+#' @param value (chr)
+#' @return Nothing.
+#' @export
+query_chronontology <- function(value = "roman") {
+  .Defunct(msg = "'query_chronontology()' has been removed from this package.",
+           package = "chrongler")
+}
